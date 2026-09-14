ccb19b480d2edf47b46ef1962dfb378335028dc4
Atreyi Prasad <149876041+Atreyiprasad@users.noreply.github.com>
2026-09-15 00:11:51 +0530

    Route alert details through investigation drawer

--- frontend/src/app/routes.tsx
+++ frontend/src/app/routes.tsx
@@ -8,24 +8,20 @@ import RegionPage from "../features/region/RegionPage";
 import ExplorePage from "../features/map/ExplorePage";
 import FacilitiesPage from "../features/facilities/FacilitiesPage";
 import AlertsPage from "../features/alerts/AlertsPage";
-import AlertDetailPage from "../features/alerts/AlertDetailPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
   },
-
   {
     path: "/region/:regionId",
     element: <RegionPage />,
   },
-
   {
     path: "/region/:regionId/explore",
     element: <ExplorePage />,
   },
-
   {
     path: "/region/:regionId/facilities",
     element: <FacilitiesPage />,
@@ -36,9 +32,8 @@ export const router = createBrowserRouter([
   },
   {
     path: "/region/:regionId/alerts/:id",
-    element: <AlertDetailPage />,
+    element: <AlertsPage />,
   },
-
   {
     path: "*",
     element: <Navigate to="/" replace />,
